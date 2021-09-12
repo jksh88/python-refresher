c713f72086a91add7a6d41f076e1bf048ff4f401
Joohoon Kim <jayjhkim7@gmail.com>
2021-09-12 15:35:01 -0400

    map and filter

---
+++ callbacks.py
@@ -0,0 +1,19 @@
+items = [2,5,6,10]
+squared = lambda x:x**2
+squaredList = list(map(squared, items))
+print(squaredList)
+
+def multiply(x, y):
+  return x*y
+
+def add(x,y):
+  return x+y
+
+funcs = [multiply, add]
+listOfTuples = [(2,3),(4,5),(6,7)]
+for i in listOfTuples:
+  print(list(map(lambda a: a(i[0], i[1]), funcs)))
+
+numList = range(-3,3)
+negative = list(filter(lambda a: a < 0, numList))
+print(negative)
---
+++ fordebug.py
@@ -0,0 +1,8 @@
+import pdb
+
+def make_bread():
+  pdb.set_trace()
+  return "no time"
+
+print(make_bread())
+
