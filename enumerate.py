1a6a27e788a1e2ba555c9f03f3e2d4bc19b706b7
Joohoon Kim <jayjhkim7@gmail.com>
2021-09-17 21:08:35 -0400

    enumerate

---
+++ enumerate.py
@@ -0,0 +1,8 @@
+cities = ['LA', 'NY', 'Chicago', 'Seoul']
+for count, value in enumerate(cities):
+  print(count, value)
+
+
+states = ['CA', 'IL', 'GA']
+for count, value in enumerate(states):
+  print(count, value)
