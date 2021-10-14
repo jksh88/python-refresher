61c62b3318fdebe413bd88ed9099df6f04b82a41
Joohoon Kim <jayjhkim7@gmail.com>
2021-10-14 00:05:12 -0400

    args and kwargs

--- argskwargs.py
+++ argskwargs.py
@@ -24,3 +24,42 @@ def concatenate(**kwargs):
   return str
 
 print(concatenate(a='thanks', b='good', c='day', d='.'))
+
+def multiply (*args):
+  total = 1
+  for arg in args:
+    total *= arg
+  print ("type: ", type(args))
+  return total
+
+print("total: " , multiply(1,3,5))
+
+#destructure
+nums = [2, 6, 1]
+def summation(*args):
+  total = 0
+  for num in args:
+    total += num
+  return total
+print("sumOfNums: ", summation(*nums))
+
+def apply (*args, operator):
+  if operator == "*":
+    return multiply(*args)
+  elif operator == "+":
+    return summation(*args)
+  else:
+    return "invalid"
+
+print(apply(*nums, operator="*"))
+
+bob = {"name": "Bob", "age": 25}
+
+def named(**kwargs):
+  print(type(kwargs))
+  result = ''
+  for key, value in kwargs.items():
+    result += f' {key}: {value} '
+  return result
+
+print(named(**bob))
