4b841ab3e77eda70b4127feaf29f5007a5219cda
Joohoon Kim <jayjhkim7@gmail.com>
2021-10-17 12:06:12 -0400

    wip: add static and class methods to priceSum

--- priceSum.py
+++ priceSum.py
@@ -1,6 +1,6 @@
 class Store:
-  def __init__(self):
-    # self.name = name
+  def __init__(self, name):
+    self.name = name
     self.items = []
 
   def add_item(self, name, price):
@@ -14,9 +14,24 @@ class Store:
       total += item['price']
     return total
 
+  # @classmethod
+  # def franchise(self):
+    # return f"{self.name} - franchise"
   
-grocery = Store()
-grocery.add_item('bananas', 8)
-grocery.add_item('shoes', 30)
-total = grocery.stock_price()
-print(total)
+  @classmethod
+  def franchise(cls, name):
+    return f"{Store.store_details(name)} - franchise"
+
+  @staticmethod
+  def store_details(store):
+    return Store.__str__(store)
+  
+target = Store("Target")
+amazon = Store("Amazon")
+# target.add_item('bananas', 8)
+# target.add_item('shoes', 30)
+# total = target.stock_price()
+# print(total)
+print(target.franchise("Target"))
+print(target)
+print(Store.store_details("Target"))
